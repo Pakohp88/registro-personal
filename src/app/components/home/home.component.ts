3d09dbb829943f181888196d011a822c2674a99d
Francisco Hernandez Perez <franciscohernandez@lavore.com.mx>
2021-11-25 17:32:53 -0600

    Commit inicial, se agregan componentes, ruteo y tablas

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../../models/empleado';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  empleados: Empleado[] = [
+    {id: 1, nombre: "Francisco", apellidoPaterno: "Hernandez", apellidoMaterno:"Perez", edad:33, sexo: "Masculino"},
+    {id: 2, nombre: "Brenda", apellidoPaterno: "Hernandez", apellidoMaterno:"Perez", edad:30, sexo: "Femenino"}
+  ];
+
+  //empleado: Empleado = new Empleado();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/models/empleado.ts
@@ -0,0 +1,7 @@
+export class Empleado {        
+    
+    constructor(public id: number, public nombre: string, public apellidoPaterno: string,
+                public apellidoMaterno: string, public edad: number, public sexo: string) {        
+    }
+
+}
