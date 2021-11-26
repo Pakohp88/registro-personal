c6b5815330d394fe1d237e5e3e4cf6d865cfcfec
Francisco Hernandez Perez <franciscohernandez@lavore.com.mx>
2021-11-26 09:42:42 -0600

    Se agregraron las validaciones en el componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Empleado } from '../../models/empleado';
 
 @Component({
@@ -8,16 +9,78 @@ import { Empleado } from '../../models/empleado';
 })
 export class HomeComponent implements OnInit {
 
-  empleados: Empleado[] = [
-    {id: 1, nombre: "Francisco", apellidoPaterno: "Hernandez", apellidoMaterno:"Perez", edad:33, sexo: "Masculino"},
-    {id: 2, nombre: "Brenda", apellidoPaterno: "Hernandez", apellidoMaterno:"Perez", edad:30, sexo: "Femenino"}
-  ];
+  empleados: Empleado[] = [{id: 1, nombre: "Francisco", apellidoPaterno: "Hernandez", apellidoMaterno:"Perez", email:"pako@mail.com", edad:33, sexo: "Masculino"}];
+  empleado: Empleado = { id: 0, nombre: "", apellidoPaterno: "", apellidoMaterno: "", email:"",  edad: 0, sexo:""  };
+  form: FormGroup;
 
-  empleado: Empleado = { id: 0, nombre: "", apellidoPaterno: "", apellidoMaterno: "", edad: 0, sexo:""  };
+  
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
+
+  get nombreNoValido() {        
+    return this.form.get('nombre').invalid && this.form.get('nombre').touched;
+   }
+
+   get apellidoPaternoNoValido() {        
+    return this.form.get('apellidoPaterno').invalid && this.form.get('apellidoPaterno').touched;
+   }
+   
+   get apellidoMaternoaternoNoValido() {        
+    return this.form.get('apellidoMaterno').invalid && this.form.get('apellidoMaterno').touched;
+   }
+
+   get emailNoValido() {        
+    return this.form.get('email').invalid && this.form.get('email').touched;
+   }
+
+   get edadNoValido() {            
+    return this.form.get('edad').invalid && this.form.get('edad').touched;
+   }
+
+   get sexoNoValido() {        
+    return this.form.get('sexo').invalid && this.form.get('sexo').touched;
+   }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellidoPaterno: ['', [Validators.required, Validators.minLength(5)]],
+      apellidoMaterno: ['', [Validators.required, Validators.minLength(5)]],   
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      edad: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      sexo: ['', Validators.required]
+    });
+  
   }
+
+  addOrEdit() {    
+
+    if (this.form.invalid) {      
+            
+      return Object.values( this.form.controls ).forEach( control => {
+        
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched() );
+        } else {
+          control.markAsTouched();
+        }              
+      });
+      
+      /*if (this.empleado.id === 0) {
+        this.empleado.id = this.empleados.length + 1;
+        this.empleados.push(this.empleado);
+      }
+      this.empleado = { id: 0, nombre: "", apellidoPaterno: "", apellidoMaterno: "", email: "", edad: 0, sexo: "" };*/
+    }
+  }
+
+  editar(e: Empleado){
+    this.empleado = e;
+  }
+
+  borrar(e: Empleado){
+    this.empleados = this.empleados.filter(x => x != e);
+  }
+  
 
 }
--- src/app/models/empleado.ts
+++ src/app/models/empleado.ts
@@ -1,7 +1,7 @@
 export class Empleado {        
     
     constructor(public id: number, public nombre: string, public apellidoPaterno: string,
-                public apellidoMaterno: string, public edad: number, public sexo: string) {        
+                public apellidoMaterno: string, public email:string, public edad: number, public sexo: string) {        
     }
 
 }
